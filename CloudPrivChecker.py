da2d1008a9d1639f72a19061219f6d35a39350ac
yogisec <yogisec7@gmail.com>
2018-06-20 09:05:47 -0500

    initial

---
+++ CloudPrivChecker.py
@@ -0,0 +1,55 @@
+import boto3
+import json
+import re
+
+iamclient = boto3.client('iam')
+
+def checkPermissions(version,arn):
+    #pull the policy details to determine what rights are available.
+    policyDetails = iamclient.get_policy_version(PolicyArn=arn,VersionId=version)
+
+    #TODO loop through this and make it pretty, and then start building intelligence 
+    print(policyDetails)
+
+def IAM():
+    print('Beginning IAM enumeration, please wait this may take awhile.')
+
+    #Determine available roles within the account
+    print('####################################################')
+    print('################ Available Roles ###################')
+    print('####################################################')
+
+    availableRoles = iamclient.list_roles()
+    for role in availableRoles['Roles']:
+        print('RoleName: ' + role['RoleName'])
+        print('Description: ' + role['Description'])
+        print('Effect: ' + role['AssumeRolePolicyDocument']['Statement'][0]['Effect'])
+        print('Service: ' + role['AssumeRolePolicyDocument']['Statement'][0]['Principal']['Service'])
+        print('Arn: ' + role['Arn'] + ' RoleId: ' + role['RoleId'])
+        print('\n')
+
+    print('Policies Currently Applied Here')
+    #pull policies applied 'locally' 
+    appliedPolicies = iamclient.list_policies(Scope='Local')
+    for policy in appliedPolicies['Policies']:
+        version = policy['DefaultVersionId']
+        arn = policy['Arn']
+
+        print('PolicyName: ' + policy['PolicyName'])
+        print('PolicyId: ' + policy['PolicyId'] + 'Arn: ' + arn)
+        print('Verion: ' + str(version))
+        checkPermissions(version,arn)
+
+def Main():
+    platform = input('Which platform are we in? \n \
+    1: AWS \n 2: GCP \n 3: Azure')
+
+    if platform == 1:
+        awareness = AWSAwarness.IAM()
+        #users = AWSAwarness.GetUsers()
+    else:
+        print('Still building')
+
+if __name__ == '__main__':
+    Main()
+
---
+++ README.md
@@ -0,0 +1,5 @@
+This project is designed to check various priviledges within cloud environments and determine what routes may exist to elevate priviledges virtically in the cloud stack.
+
+Right now this is script is designed to be used on an EC2 or somehwere within AWS where there is an interactive shell. Utilizing any permissions that are inherited.
+
+This project is under development. For a more completed project you should check out Rhino Security Labs' aws_escalate.py utility at: https://github.com/RhinoSecurityLabs/Security-Research/blob/master/tools/aws-pentest-tools/aws_escalate.py
